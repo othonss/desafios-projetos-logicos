ed0187f13991dc5959bad91832629a43deb210b9
othonss <othon.santos.silva@gmail.com>
2022-09-24 20:08:04 -0300

    CRIAÇÃO DO SCRIPT DO BANCO DE DADOS Foi criado o banco de dados ecommerce Foram criadas as tabelas cliente e produto

--- primeiro_projeto_logico/projeto_logico_bd.sql
+++ primeiro_projeto_logico/projeto_logico_bd.sql
@@ -1,2 +1,27 @@
 -- CRIAÇÃO DO BANCO DE DADOS PARA O CENÁRIO E-COMMERCE
 create database ecommerce;
+use ecommerce;
+
+-- CRIAR TABELA CLIENTE
+create table cliente (
+	idClient int auto_increment primary key,
+    Fname varchar(10),
+    Minit char(3),
+    Lname varchar(20),
+    CPF char(11) not null,
+    Address varchar(30),
+    constraint unique_cpf_client unique (CPF)
+);
+
+-- CRIAR TABELA PRODUTO
+create table produtc (
+	idProduct int auto_increment primary key,
+    Pname varchar(10) not null,
+    Classification_kids bool default false,
+    Category enum('Eletrônico', 'Vestimenta', 'Brinquedos', 'Alimentos', 'Móveis') not null,
+    Avaliacao float default 0,
+    Size varchar(10),
+    constraint unique_cpf_cliente unique (CPF)
+);
+
+-- CRIAR TABELA PEDIDO
